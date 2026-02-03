d65c3efa4dbfc213949358394dd2123fc0866bbd
William Chen <william_w_chen@trendmicro.com>
2026-02-04 01:05:36 +0800

    add WalletConfig and update WalletPaymentService to use WalletConfig for payment configuration

---
+++ src/main/kotlin/com/module/multitenantbookingservice/core/tenant/config/payment/WalletConfig.kt
@@ -0,0 +1,3 @@
+package com.module.multitenantbookingservice.core.tenant.config.payment
+
+class WalletConfig {}
--- src/main/kotlin/com/module/multitenantbookingservice/payment/vendor/wallet/WalletPaymentService.kt
+++ src/main/kotlin/com/module/multitenantbookingservice/payment/vendor/wallet/WalletPaymentService.kt
@@ -1,12 +1,19 @@
 package com.module.multitenantbookingservice.payment.vendor.wallet
 
+import com.module.multitenantbookingservice.core.tenant.config.payment.WalletConfig
 import com.module.multitenantbookingservice.payment.PaymentService
 import com.module.multitenantbookingservice.payment.model.PaymentCallback
 import com.module.multitenantbookingservice.payment.model.PaymentInitiationResult
 import com.module.multitenantbookingservice.payment.model.PaymentRequest
 import com.module.multitenantbookingservice.payment.model.PaymentResult
 
-class WalletPaymentService: PaymentService {
+
+
+class WalletPaymentService: PaymentService<WalletConfig> {
+    override fun getPaymentConfig(): WalletConfig {
+        TODO("Not yet implemented")
+    }
+
     override fun initiatePayment(request: PaymentRequest): PaymentInitiationResult {
         TODO("Not yet implemented")
     }
