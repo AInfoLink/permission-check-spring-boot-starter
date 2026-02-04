2bdf06f9586bb1e7d4edf12387d8d24c737c48ae
William Chen <william_w_chen@trendmicro.com>
2026-02-04 13:37:26 +0800

    add MembershipPricing entity and update Membership to include pricing association

--- src/main/kotlin/com/module/multitenantbookingservice/core/models/Membership.kt
+++ src/main/kotlin/com/module/multitenantbookingservice/core/models/Membership.kt
@@ -3,7 +3,9 @@ package com.module.multitenantbookingservice.core.models
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToMany
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import java.util.UUID
 
@@ -21,7 +23,24 @@ class Membership(
     @Column(name = "description", length = 500)
     val description: String? = null,
 
-
     @OneToMany(mappedBy = "membership")
-    val profiles: MutableSet<UserProfile> = mutableSetOf()
+    val profiles: MutableSet<UserProfile> = mutableSetOf(),
+
+    @OneToOne(mappedBy = "membership")
+    val pricing: MembershipPricing,
+)
+
+
+@Entity
+@Table(name = "membership_pricings")
+class MembershipPricing(
+    @Id
+    val id: UUID = UUID.randomUUID(),
+
+    @OneToOne
+    @JoinColumn(name = "membership_id", nullable = false)
+    val membership: Membership,
+
+    @Column(name = "discount_percentage", nullable = false)
+    val discountPercentage: Double,
 )
